f646181b50f08bfa3fe835fc7fbbcdf399cf926b
Kuman13 <kyym13@gmail.com>
2023-09-20 21:32:29 +0200

    home_work1

---
+++ script/script.js
@@ -0,0 +1,39 @@
+// alert('Зовнішний файл');
+
+// console.log('Externel file');
+// console.log(    'Hello,world'    );
+
+// document.write('Some String')
+
+// /* console.log('Hello,world');
+// console.log('Hello,world');
+// console.log('Hello,world');
+// console.log('Hello,world'); */
+// console.log(1234);
+// console.log('1234');
+
+
+// x = 333;
+
+// console.log(x);
+
+// stroka = 'any string';
+
+// console.log(stroka);
+
+// alert('Alert');
+
+// num = prompt('Enter number: ', 0);
+
+// console.log(num);
+
+// document.write(num);
+
+// alert(num);
+
+
+name = prompt("What's your name?:");
+message = "Hello, " + name + "! How are you?";
+console.log(message)
+document.write(message)
+alert(message);
